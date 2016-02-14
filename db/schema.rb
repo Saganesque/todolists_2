68413346bfe039bf2697b27ca7a6a3ba7eedc6de
Saganesque <forvendorsonly@gmail.com>
2016-02-14 11:27:55 -0800

    First four tests pass now, and the migrations are tracked.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214023323) do
+ActiveRecord::Schema.define(version: 20160214192515) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "gender"
