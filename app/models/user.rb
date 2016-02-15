0d17d319ce104b037d8e0963116c5a14bc74773c
Saganesque <forvendorsonly@gmail.com>
2016-02-14 20:46:27 -0800

    8 tests passing, but I'm iffy about rs08. The reuse of one symbol concerns me.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,24 @@
 class User < ActiveRecord::Base
-  has_one :profile, dependent: :destroy
-  has_many :todo_lists, dependent: :destroy
+
+
+=begin
+8. Implement a 1:many :through relationship from User to TodoItem by using the 1:many relationship from User to
+
+TodoLists as a source.
+
+  User
+
+todo_items - a 1:many through relationship with TodoItem through TodoLists (i.e., User has_many
+
+todo_items)
+=end
+
+
+has_one :profile, dependent: :destroy
+has_many :todo_lists, dependent: :destroy
+
+has_many :todo_items, through: :todo_lists, source: :todo_items
 
 end
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+=begin
+
+Create a seeds.rb file that will clear the existing data from the model tables and load the database with
+
+The four users below with their birth years:
+
+                                          Carly Fiorina, 1954
+
+Donald Trump, 1946
+
+Ben Carson, 1951
+
+Hillary Clinton, 1947
+
+Usernames (e.g., their last names) and a password for each User
+
+                                                    A Profile for each User
+
+                                                                Exactly one TodoList per User that is due one year from the date the database is loaded
+
+                                                                (hint: Date.today provides today's date and 1.year can be used to define one year)
+
+Each TodoList contains five (5) TodoItems (there must be 20 total)
+
+Each TodoItem having a due date of one year from the time the database is loaded
+
+Each TodoItem must have an arbitrary title and descriptions
+
+(Hint: you may want to consider using loops)
+
+Once the seeds.rb file is created, populate the database using rake db:seed
+
+=end
+
+
+
+
+User.destroy_all
+
+User.create![
+{Carly Fiorina, 1954}
+{Donald Trump, 1946}
+{Ben Carson, 1951}
+{Hillary Clinton, 1947}
+]
