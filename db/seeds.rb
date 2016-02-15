44f482838b80cb6c0c1585c93ae514d3204c30ff
Saganesque <forvendorsonly@gmail.com>
2016-02-15 00:26:59 -0800

    Progress on seed.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,10 +44,50 @@ Once the seeds.rb file is created, populate the database using rake db:seed
 
 
 User.destroy_all
+Profile.destroy_all
+TodoList.destroy_all
+TodoItem.destroy_all
 
-User.create![
-{username: "Fiorina", password_digest: "1954"},
-{username: "Trump", password_digest: "1946"},
-{username: "Carson", password_digest: "1951"},
-{username: "Clinton", password_digest: "1947"}
+User.create! [
+    {username: "Fiorina", password_digest: "1954"},
+    {username: "Trump", password_digest: "1946"},
+    {username: "Carson", password_digest: "1951"},
+    {username: "Clinton", password_digest: "1947"}
 ]
+
+Profile.create! [
+    {gender: "female", birth_year: "1954", first_name: "Carly", last_name: "Fiorina"},
+    {gender: "male", birth_year: "1946", first_name: "Donald", last_name: "Trump"},
+    {gender: "male", birth_year: "1951", first_name: "Ben", last_name: "Carson"},
+    {gender: "female", birth_year: "1947", first_name: "Hillary", last_name: "Clinton"}
+                ]
+TodoList.create! [
+                     {list_name: "td1", list_due_date: "1-1-2001"},
+                     {list_name: "td2", list_due_date: "1-1-2002"},
+                     {list_name: "td3", list_due_date: "1-1-2003"},
+                     {list_name: "td4", list_due_date: "1-1-2004"}
+                 ]
+
+=begin
+it "must have Profiles set up for each user with the given data" do
+  expect(Profile.all.length).to be(4)
+  user_list.each do | fname, lname, gender, byear |
+    p = Profile.find_by(last_name: lname)
+    expect(p.first_name).to eql(fname)
+    expect(p.gender).to eql(gender)
+    expect(p.birth_year).to eql(byear)
+  end
+end
+
+    create_table :todo_lists do |t|
+      t.string :list_name
+      t.date :list_due_date
+
+      t.timestamps null: false
+
+
+t.string :gender
+t.integer :birth_year
+t.string :first_name
+t.string :last_name
+=end
