e7981bb9056d62506e31171a43b2f7ee4088824a
Saganesque <forvendorsonly@gmail.com>
2016-02-16 12:59:25 -0800

    rq09 passing

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,49 +43,25 @@ Profile.create! [
     {gender: "female", birth_year: "1947", first_name: "Hillary", last_name: "Clinton"}
                 ]
 
-#tmp_tdl_array = Array.new(0)
-#tmp_tdi_array = Array.new(0)
+
 
 User.all.each do |x|
-  #tmp_tdl_array.push ({list_name: "#{x.username}_tdl", list_due_date: Date.today})
- a1= TodoList.create! ({list_name: "#{x.username}_tdl", list_due_date: Date.today})
- x.todo_lists << a1
- aaaaa=1
-  while aaaaa < User.count
-  #tmp_tdi_array.push ({due_date: Date.tomorrow, title: "item_#{a} based on #{x.username}", description: "something", completed: false})
+
+  a1= TodoList.create! ({list_name: "#{x.username}_tdl", list_due_date: Date.today})
+  x.todo_lists << a1
+
+
+  5.times do |aaaaa|
+
+
    a2= TodoItem.create! ({due_date: Date.tomorrow, title: "item_#{aaaaa} based on #{x.username}", description: "something", completed: false})
    a1.todo_items << a2
-p a1.todo_items
-   aaaaa+=1
+   a1.todo_items
   end
 
-end
 
-#TodoList.create! tmp_tdl_array
-
-
-
-
-=begin
-it "must have Profiles set up for each user with the given data" do
-  expect(Profile.all.length).to be(4)
-  user_list.each do | fname, lname, gender, byear |
-    p = Profile.find_by(last_name: lname)
-    expect(p.first_name).to eql(fname)
-    expect(p.gender).to eql(gender)
-    expect(p.birth_year).to eql(byear)
   end
-end
-
-    create_table :todo_lists do |t|
-      t.string :list_name
-      t.date :list_due_date
-
-      t.timestamps null: false
 
 
-t.string :gender
-t.integer :birth_year
-t.string :first_name
-t.string :last_name
-=end
+
+
