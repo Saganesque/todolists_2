92d4da17ef39193b4d746a81c48dce27ba605507
Saganesque <forvendorsonly@gmail.com>
2016-02-17 13:25:54 -0800

    rq 12 passing

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,38 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+  validates_inclusion_of :gender, in: %w( male female )
+  validate :my_happy_fname_lname_null_validator
+  validate :no_sue
+
+def  my_happy_fname_lname_null_validator
+
+  if first_name.nil? && last_name.nil?
+    then
+  errors.add(:first_name, "Only one of either first name or last name may be empty")
+  errors.add(:last_name, "Only one of either first name or last name may be empty")
+  end
+
+  end
+def no_sue
+  if gender == "male" && first_name== "Sue"
+  then
+    errors.add(:gender, "The man in black has a quarrel with you.")
+  end
+
+end
+
+
+=begin
+
+
+    Profile
+
+    Define custom validator that permits first_name or last_name to be null but not both
+
+    Define a validation for gender to be either "male" or "female" by using a built-in validator
+
+    Define custom validator that prevents anyone that is male (gender) from having the first_name "Sue" ;)
+
+=end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,25 @@ class User < ActiveRecord::Base
 
 
 =begin
+  13. Add a method to the User model class called get_completed_count, which:
+
+      determines the number of TodoItems the User has completed using an aggregate query function
+
+  (Hint: You are looking for the count of TodoItems associated with a specific User where
+
+                           completed:true)
+
+                           returns the count
+=end
+
+  def get_completed_count
+
+  end
+
+
+
+
+=begin
 8. Implement a 1:many :through relationship from User to TodoItem by using the 1:many relationship from User to
 
 TodoLists as a source.
@@ -18,6 +37,15 @@ has_one :profile, dependent: :destroy
 has_many :todo_lists, dependent: :destroy
 
 has_many :todo_items, through: :todo_lists, source: :todo_items
+
+validates_presence_of :username
+=begin
+
+11. Add validation to User and Profile models
+
+User
+
+=end
 
 end
 
