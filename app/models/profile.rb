0c0dad3e1ac9e1790864fc2367edc4d65c9be0c3
Saganesque <forvendorsonly@gmail.com>
2016-02-17 21:32:28 -0800

    All tests passing

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,6 +3,24 @@ class Profile < ActiveRecord::Base
   validates_inclusion_of :gender, in: %w( male female )
   validate :my_happy_fname_lname_null_validator
   validate :no_sue
+  default_scope {order birth_year: :asc}
+
+
+=begin
+ Add a class method to the Profile class, called get_all_profiles, which:
+accepts a min and max for the birth year
+issues a BETWEEN SQL clause in a where clause to locate Profiles with birth years that are between min
+year and max year
+defends itself against SQL injection when applying the parameters to the SQL clauses
+returns a collection of Profiles in ASC birth year order
+=end
+
+def self.get_all_profiles (min,max)
+
+  Profile.where(:birth_year =>  min..max).to_a
+
+
+end
 
 def  my_happy_fname_lname_null_validator
 
@@ -22,17 +40,6 @@ def no_sue
 end
 
 
-=begin
 
-
-    Profile
-
-    Define custom validator that permits first_name or last_name to be null but not both
-
-    Define a validation for gender to be either "male" or "female" by using a built-in validator
-
-    Define custom validator that prevents anyone that is male (gender) from having the first_name "Sue" ;)
-
-=end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,7 @@
 class User < ActiveRecord::Base
 
 
-=begin
-  13. Add a method to the User model class called get_completed_count, which:
 
-      determines the number of TodoItems the User has completed using an aggregate query function
-
-  (Hint: You are looking for the count of TodoItems associated with a specific User where
-
-                           completed:true)
-
-                           returns the count
-=end
 
   def get_completed_count
     self.todo_items.where(completed: true).count
@@ -21,33 +11,12 @@ class User < ActiveRecord::Base
 
 
 
-
-=begin
-8. Implement a 1:many :through relationship from User to TodoItem by using the 1:many relationship from User to
-
-TodoLists as a source.
-
-  User
-
-todo_items - a 1:many through relationship with TodoItem through TodoLists (i.e., User has_many
-
-todo_items)
-=end
-
-
 has_one :profile, dependent: :destroy
 has_many :todo_lists, dependent: :destroy
 
 has_many :todo_items, through: :todo_lists, source: :todo_items
 
 validates_presence_of :username
-=begin
-
-11. Add validation to User and Profile models
-
-User
-
-=end
 
 end
 
